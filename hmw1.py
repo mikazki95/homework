f533f120abf0193038b6ff7504cb387d95c3b46e
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2023-11-29 09:17:10 -0600

    Update hmw1.py
    complite

--- hmw1.py
+++ hmw1.py
@@ -4,17 +4,23 @@ def NumeroBinario(numero):
     representación binaria. Debe recibir y devolver un valor de tipo entero.
     En caso de que el parámetro no sea de tipo entero y mayor a -1 retorna nulo.
     '''
-    result = []
+    aux_1 = []
+    aux_2 =[]
+    result = ""
     zero =0
     if (numero>=0):
-        zero = numero/2 
-        if (zero>1):
-            if (zero%2):
-                result.append(0)
+        while numero > 0 :
+            if (numero>2):
+                if (numero%2 == 0):
+                    aux_1.append(0)
+                else:
+                    aux_1.append(1)
             else:
-                result.append(1)
-        else:
-            return result    
+                aux_1.append(1)
+                aux_2 = aux_1[::-1]
+                result="".join(str(x) for x in aux_2)
+                return result   
+            numero = int (numero/2 )
     else:
         print("el numero debe ser mayor o igual a cero para convertir")
     
